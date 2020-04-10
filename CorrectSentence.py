9f5dc1f9ad9b7a1bc98ab5c4c1247358b4d2a02f
Ignas Markauskas <ignasmark5@gmail.com>
2020-04-10 20:19:50 +0200

    correct sentence

---
+++ CorrectSentence.py
@@ -0,0 +1,14 @@
+#%%
+def correct_sentence(text):
+    if text[0].islower():
+        text = text.replace(text[0], text[0].swapcase(), 1)
+    if not text.endswith('.'):
+        text += '.'
+    return text
+
+# Running some test...
+print(correct_sentence("greetings, friends") == "Greetings, friends.")
+print(correct_sentence("Greetings, friends") == "Greetings, friends.")
+print(correct_sentence("Greetings, friends.") == "Greetings, friends.")
+print(correct_sentence("hi") == "Hi.")
+print(correct_sentence("welcome to New York") == "Welcome to New York.")
