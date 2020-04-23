7af41d726ce6b3f3ffb08be375eeabc99045774e
Ignas Markauskas <ignasmark5@gmail.com>
2020-04-23 20:10:50 +0200

    Initial

---
+++ endZeros.py
@@ -0,0 +1,17 @@
+# End Zeros
+def end_zeros(num):
+    result = 0
+    for i in str(num)[::-1]:
+        if int(i) == 0:
+            result = result + 1
+        else:
+            break
+    return result
+
+# Running some tests..
+print(end_zeros(0) == 1)
+print(end_zeros(1) == 0)
+print(end_zeros(10) == 1)
+print(end_zeros(101) == 0)
+print(end_zeros(245) == 0)
+print(end_zeros(100100) == 2)
