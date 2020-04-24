3ed0fdb36a54efe4df038f1df7efdf7cc7d16ea8
Ignas Markauskas <ignasmark5@gmail.com>
2020-04-24 20:10:50 +0200

    Initial

---
+++ positiveNumber.py
@@ -0,0 +1,9 @@
+# Number Length
+def number_length(a: int) -> int:
+    return len(str(a))
+
+# Running some tests..
+print(number_length(10) == 2)
+print(number_length(0) == 1)
+print(number_length(4) == 1)
+print(number_length(44) == 2)
