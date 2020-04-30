26c7d7ab516644655b4adcb55663f508efd5bc71
Ignas Markauskas <ignasmark5@gmail.com>
2020-04-30 20:10:50 +0200

    first

---
+++ maxDigit.py
@@ -0,0 +1,10 @@
+# Max Digit
+def max_digit(number):
+    return int(max(str(number)))
+
+# Running some tests..
+print(max_digit(0) == 0)
+print(max_digit(52) == 5)
+print(max_digit(634) == 6)
+print(max_digit(1) == 1)
+print(max_digit(10000) == 1)
